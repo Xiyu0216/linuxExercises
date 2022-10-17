a2755dd69aec6174da5db1fbe5c919f7eefcdd1f
Xiyu Hao <xhao33@wisc.edu>
2022-10-17 11:27:40 -0500

    make changes to five_dirs.sh

--- five_dirs.sh
+++ five_dirs.sh
@@ -1,1 +1,13 @@
 #!/bin/bash
+
+rm -rf /five
+mkdir five
+mkdir five/dir{1..5}
+
+for i in {1..5}; do
+    for j in {1..4}; do
+	for k in $j; do
+	    echo $j >> five/dir$i/file$j;
+	done;
+    done;
+done;
