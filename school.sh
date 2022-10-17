0041d5e5f3133b74efe125fe1a59106980f38332
Jiaqi Xia <jxia53@wisc.edu>
2022-10-17 10:28:13 -0500

    changes of q3.

--- school.sh
+++ school.sh
@@ -1,1 +1,11 @@
 #!/bin/bash
+taxes=$(cat Property_Tax_Roll.csv | grep "MADISON SCHOOLS" | cut -d ',' -f7)
+sum=0
+counter=0
+for i in $taxes
+do
+  sum=$(($sum+$i))
+  counter=$(($counter+1))
+done
+average=$((sum/$counter))
+echo "the average of the TotalAssessedValue $average"
