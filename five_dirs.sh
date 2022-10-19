b2abde4fe3db49c8b889f872745138041568056b
Jiaqi Xia <jxia53@wisc.edu>
2022-10-19 08:33:11 -0500

    make changes to school and five_dirs

--- five_dirs.sh
+++ five_dirs.sh
@@ -3,12 +3,12 @@
 mkdir five
 
 for i in {1..5}; do
-mkdir five/dir{1..5}
+mkdir five/dir${i}
     for j in {1..4}; do
-	for k in $(seq 1 $j); do
+	for k in $(seq 1 ${j}); do
 
-	    echo $j >> five/dir$i/file$j;
-		
+	    echo ${j} >> five/dir${i}/file${j};
+
 	done;
     done;
 done;
--- school.sh
+++ school.sh
@@ -1,11 +1,2 @@
 #!/bin/bash
-taxes=$(cat Property_Tax_Roll.csv | grep "MADISON SCHOOLS" | cut -d ',' -f7)
-sum=0
-counter=0
-for i in $taxes
-do
-  sum=$(($sum+$i))
-  counter=$(($counter+1))
-done
-average=$((sum/$counter))
-echo "the average of the TotalAssessedValue $average"
+head -5 Property_Tax_Roll.csv | grep "MADISON SCHOOL" | cut -d, -f 7 | { sum=0 n=0; while read i; do sum=$(($sum+$i)) n=$(($n+1)); done; echo $(($sum/$n)); }
