d74f6f59fba8e1c82f5bd94ecc401f041b01f43d
Xiyu Hao <xhao33@wisc.edu>
2022-10-19 08:55:48 -0500

    make changes to five_dirs.sh

--- five_dirs.sh
+++ five_dirs.sh
@@ -2,12 +2,10 @@
 
 mkdir five
 
-for i in {1..5}
-do
-mkdir five/dir{1..5}
-for j in {1..4}
-do
-	for k in $(seq 1 $j)
+for i in {1..5}; do
+mkdir five/dir${i}
+for j in {1..4}; do
+	for k in $(seq 1 $j);
 	do
 
 	    echo $j >> five/dir$i/file$j;
